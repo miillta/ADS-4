aa5e4e933e8a55a21c8d0a70fce8c01c51182513
MilanaTarasova <150841920+miillta@users.noreply.github.com>
2024-03-22 16:32:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,36 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value) {
+  int count = 0;
+	for (int i = 0; i < len; i++) {
+		for (int j = i+1; j < len; j++)  {
+			if ((arr[i] + arr[j]) == value) count++;
+		}
+	}
+	return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+	int count = 0;
+	int endl = len - 1;
+	while (arr[endl] > value) {
+		endl--;
+	}
+	for (int i = 0; i < endl; i++) {
+		for (int j = endl; i < j; j--) {
+			if ((arr[i] + arr[j]) == value) count++;
+		}
+	}
+	return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int* arr, int len, int value) {
+	int endl = len - 1;
+	int count = 0;
+	while (arr[endl] > value) endl--;
+	for (int i = 0; i < endl; i++) {
+		for (int j = endl; i < j; j--) {
+			if (((arr[i] + arr[j]) == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+				count++;
+			}
+		}
+	}
+	return count;
 }
